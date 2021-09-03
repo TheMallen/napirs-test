15137005244d4d7a153b74c92066d9307cf96e14
Mallory Allen <mathew.allen@shopify.com>
2021-09-03 14:23:06 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ use napi::{Env, JsObject, Result};
 #[module_exports]
 fn init(mut exports: JsObject, _env: Env) -> Result<()> {
   exports.create_named_method("fibonacci", fibonacci)?;
+  // testing CI
   // exports.set_named_property("DEFAULT_VALUE", env.create_int64(100)?)?;
   Ok(())
 }
